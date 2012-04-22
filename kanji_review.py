ebb75d40443bef32c9bfb68affaff637d3399728
Bruno Azzinnari <brazzinnari@gmail.com>
2012-04-22 02:28:44 -0700

    Buffering for removing delay in getting next kanji (only Review Kanji mode for now).

--- kanji_review.py
+++ kanji_review.py
@@ -20,25 +20,38 @@ class KanjiReview(object):
 		self.max_key = max (self.kanjis.keys() )
 	
 	@cherrypy.expose
-	def random_kanji(self, min_index = -1, max_index = 99999):
+	def random_kanji(self, amount = 1, min_index = -1, max_index = 99999):
 		# parse arguments
 		try:
 			min_index = int(min_index)
 			max_index = int(max_index)
+			amount = int(amount)
 		except ValueError:
-			return "Error: invalid value in parameters 'min_index' and 'max_index'."
+			return "Error: invalid value in parameters 'amount', 'min_index' and/or 'max_index'."
 
 		cherrypy.response.headers['Content-Type'] = "application/json"
 
+		# sanitize parameters
 		min_index = max(min_index, 1)
 		max_index = min(max_index, self.max_key)
-		key = -1
-		# since not all key values may be present:
-		while True:
-			key = random.randint(min_index, max_index)
-			if key in self.kanjis.keys():
-				break
-		# return the info
-		info = {'index' : key, 'kanji' : self.kanjis[key][0], 'meaning' : self.kanjis[key][1]}
-		return json.dumps(info)
+		amount = max(1, amount)
+
+		pack = []
+		for i in range(0, amount):
+			info = self.get_random_kanji(min_index, max_index)
+			pack.append(info)
+
+		return json.dumps(pack)
+
+	def get_random_kanji(self, min_index, max_index):
+		kanji = 'NO KANJI'
+		meaning = 'invalid data'
+		
+		key = random.randint(min_index, max_index)
+		if key in self.kanjis.keys():
+			kanji = self.kanjis[key][0]
+			meaning = self.kanjis[key][1]
+		
+		return {'index' : key, 'kanji' : kanji, 'meaning' : meaning}
+
 
